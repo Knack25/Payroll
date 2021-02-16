b8e857a75bce01b8be0288f746ea27dcba37621e
Nathan Schneider <nathangschneider@gmail.com>
2021-02-15 20:47:16 -0500

    Added basic frame functionality

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import gui.MyFrame;
 import gui.main_menu;
 import data.Employee;
 
@@ -5,6 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		MyFrame main_menu = new MyFrame("ACI");
+		
+		while (main_menu.isVisible() == false) {
+			
+			System.out.println("Main Menu Closed");
+			
+		}
+		
+		
 	}
 
 }
