34b224ea68dbac69d17408755fe5158eba9176af
Nathan Schneider <nathangschneider@gmail.com>
2021-03-02 16:30:33 -0500

    Added Automatic Disconnection from the MySQL Database when the main window is closed. Added Framework for all button press actions on the Main Menu. Moved All classes pertaining to Data to the Data package. Cleaned up Main.

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -1,13 +1,30 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.Connection;
 
 import javax.swing.*;
 
-public class MainMenu {
+import sql.MySQL;
 
-	public static JFrame createMainMenu() {
+public class MainMenu extends JFrame implements ActionListener{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void createMainMenu() {
+		
+		
+		//Connect to the database
+		Connection conn = MySQL.Connect();
+				
+		System.out.println("Successful Connectin to DB!");
 		
 		JFrame MainMenu = new JFrame("ACI Payroll");
 		MainMenu.setLayout(new BorderLayout());
@@ -17,21 +34,29 @@ public class MainMenu {
 		JPanel panelPayroll = new JPanel(new BorderLayout());
 		JPanel panelEmployee = new JPanel(new BorderLayout());
 		
-		JButton configB = new JButton();
-		configB.setText("View Configuration");
+		JButton configB = new JButton("View Configuration");
+		configB.addActionListener(configBListener);
 		
 		JLabel payrollL = new JLabel("Payroll");
-		JButton processPayroll = new JButton();
-		processPayroll.setText("Process Payroll");
+		JButton processPayroll = new JButton("Process Payroll");
+		processPayroll.addActionListener(processPayrollBListener);
 		JButton historyPayroll = new JButton("View Payroll History");
+		historyPayroll.addActionListener(historyPayrollBListener);
 		JButton voidPayroll = new JButton("Void a Check");
+		voidPayroll.addActionListener(voidPayrollBListener);
 		JButton printReports = new JButton("Print Reports");
+		printReports.addActionListener(printReportsBListener);
 		
 		
 		JLabel empL = new JLabel("Employee History");
 		JButton createEmployee = new JButton("Create Employee");
+		createEmployee.addActionListener(createEmpBListener);
 		JButton editEmployee = new JButton("Edit existing Employee");
+		editEmployee.addActionListener(editEmpBListener);
 		JButton removeEmployee = new JButton("Remove existing Employee");
+		removeEmployee.addActionListener(removeEmpBListener);
+		
+		
 		
 		panelOption.add(optionL,BorderLayout.NORTH);
 		panelOption.add(configB,BorderLayout.EAST);
@@ -48,12 +73,87 @@ public class MainMenu {
 		MainMenu.add(panelOption,BorderLayout.NORTH);
 		MainMenu.add(panelPayroll,BorderLayout.CENTER);
 		MainMenu.add(panelEmployee,BorderLayout.SOUTH);
-		MainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		MainMenu.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		MainMenu.setSize(800,800);
 		MainMenu.setIconImage(icon.getImage());
 		
 		
+		
+		
 		MainMenu.setVisible(true);
-		return MainMenu;
-		}	
+		
+		MainMenu.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.out.println("Main Window Clossed...");
+				System.out.println("Disconnecting from Database...");
+				//Close the connection
+				MySQL.CloseConnection(conn);
+			}
+		});
+		
+		return;
+		}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}	
+	
+	static ActionListener configBListener = new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			String str = event.getActionCommand();
+			System.out.println("Clicked = " + str);
+		}
+	};
+	
+	static ActionListener processPayrollBListener = new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			String str = event.getActionCommand();
+			System.out.println("Clicked = " + str);
+		}
+	};
+	
+	static ActionListener historyPayrollBListener = new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			String str = event.getActionCommand();
+			System.out.println("Clicked = " + str);
+		}
+	};
+	
+	static ActionListener voidPayrollBListener = new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			String str = event.getActionCommand();
+			System.out.println("Clicked = " + str);
+		}
+	};
+	
+	static ActionListener printReportsBListener = new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			String str = event.getActionCommand();
+			System.out.println("Clicked = " + str);
+		}
+	};
+	
+	static ActionListener createEmpBListener = new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			String str = event.getActionCommand();
+			System.out.println("Clicked = " + str);
+		}
+	};
+	
+	static ActionListener editEmpBListener = new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			String str = event.getActionCommand();
+			System.out.println("Clicked = " + str);
+		}
+	};
+	
+	static ActionListener removeEmpBListener = new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+			String str = event.getActionCommand();
+			System.out.println("Clicked = " + str);
+		}
+	};
+	
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,31 +1,10 @@
 package main;
 
-import java.sql.Connection;
-
-import javax.swing.JFrame;
-
-import sql.*;
 import gui.MainMenu;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//Connect to the database
-		Connection conn = MySQL.Connect();
-		
-		System.out.println("Successful Connectin to DB!");
-		
-		JFrame mm = MainMenu.createMainMenu();
-		
-		while (mm.isActive()) {
-			
-		}
-		
-		
-		//Close the connection
-		MySQL.CloseConnection(conn);
+		MainMenu.createMainMenu();
 	}
-
 }
