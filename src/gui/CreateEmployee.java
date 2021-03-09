bf7da0541e91c2fd1381919a3ac19cc9d834f050
Dante Parente <dontaparenta99@gmail.com>
2021-03-09 14:22:36 -0500

    Created New file for the "create employee" dialogue

---
+++ src/gui/CreateEmployee.java
@@ -0,0 +1,33 @@
+package gui;
+
+ import java.awt.BorderLayout;
+ import java.awt.event.ActionEvent;
+ import java.awt.event.ActionListener;
+ import java.awt.event.WindowAdapter;
+ import java.awt.event.WindowEvent;
+ import java.sql.Connection;
+
+ import javax.swing.*;
+
+ import sql.MySQL;
+ 
+ 
+ public class CreateEmployee extends JFrame  {
+	 
+	 
+	 public static void CreateMenu() {
+		 JFrame Create = new JFrame("Create");
+		 
+		 JLabel create_EmployeeL = new JLabel("Create Employee");
+		 JButton createB = new JButton("Create");
+		 JButton deleteB = new JButton("Create");
+		 
+		 
+		 
+	 }
+	 
+	 
+	 
+	 
+	 
+ }
